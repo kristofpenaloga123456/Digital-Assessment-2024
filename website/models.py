d815c40cd19825e8ff70ba67f90526c53c44f887
kristofpenaloga123456 <164812752+kristofpenaloga123456@users.noreply.github.com>
2024-07-04 12:27:16 +1200

    Autopep8 for models.py

--- website/models.py
+++ website/models.py
@@ -2,13 +2,16 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     first_name = db.Column(db.String(150))
     password = db.Column(db.String(150))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    notes = db.relationship('Note', backref='user', lazy=True)  # Added backref and lazy loading
+    # Added backref and lazy loading
+    notes = db.relationship('Note', backref='user', lazy=True)
+
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
