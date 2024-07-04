54d8e82cabe0001de39bf3607b0a8dc7f4db933c
kristofpenaloga123456 <164812752+kristofpenaloga123456@users.noreply.github.com>
2024-07-04 12:19:31 +1200

    Autopep8 for init.py

--- website/__init__.py
+++ website/__init__.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "ak;RT2!I:L>jI!f"
@@ -12,24 +13,22 @@ def create_app():
     db.init_app(app)
 
     from .views import views
-    from .auth import auth 
-    
+    from .auth import auth
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    
+
     from .models import User, Note
-    
+
     with app.app_context():
         db.create_all()
-
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-    
-    
+
     @login_manager.user_loader
     def load_user(id):
-        return User.query.get(int(id))  
-  
+        return User.query.get(int(id))
+
     return app
